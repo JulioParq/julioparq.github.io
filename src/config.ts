1fa5e9fe06a9d3c83905afd4b7ad9961e3ed92b1
Julio Pérez <distruccion.dev@gmail.com>
2024-08-06 16:07:49 -0700

    deploy1

---
+++ src/config.ts
@@ -0,0 +1,149 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://julioparq.github.io", // replace this with your deployed domain
+  author: "Julio Pérez",
+  profile: "https://julioparq.github.io",
+  desc: "Personal page Julio Parq",
+  title: "Julio Parq",
+  ogImage: "JP.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "es", // html lang code. Set this empty and default will be "en"
+  langTag: ["es-Es"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: true,
+  svg: true,
+  width: 35,
+  height: 35,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/JulioParq",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/zagenvan",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/julioparq/",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: false,
+  },
+  {
+    name: "Mail",
+    href: "mailto:julioparq81@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/zagenvan",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://api.whatsapp.com/send?phone=3112049659",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: true,
+  },
+  {
+    name: "Snapchat",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://julioparq.github.io",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/pages/bio.md
@@ -0,0 +1,35 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "Bio"
+---
+
+<div class= place-content-start >
+<img src="/assets/me.webp" class="mx-auto imgMe" alt="me">
+</div>
+
+Julio Pérez is an accomplished architect with over a decade of experience in the field. His creative and curious nature, coupled with his exceptional capacity for analysis and abstract thinking, allows him to approach architectural challenges from unique and innovative perspectives.
+
+Transdisciplinary expertise, enriched by his studies in mathematics and the arts, enables him to integrate diverse concepts and techniques into his designs. This holistic approach not only enhances the aesthetic and functional quality of his projects but also ensures they are thoughtfully tailored to meet the specific needs and desires of his clients.
+
+## Education
+
+### MA in Archictect
+**UdeC** / 2011 - 2012 📍 Celaya, Gto.
+
+### BSc in Math
+**UnADM** / 2018 - 2022 🌐 online
+
+### BA in Visual Art
+**UAQ** / 2002 - 2006 📍 Querétaro, Qro..
+
+
+[download CV](../CV_JCPG.pdf)
+
+<style>
+.imgMe{
+    width: 120px;
+    height: auto;
+    float: left;
+    position: relative;
+}
+</style>
